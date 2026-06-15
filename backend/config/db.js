440b46aeaa4eacb023d77b50a689d4d084b35ed8
Luckychh13 <luckychh005@gmail.com>
2026-06-15 00:33:35 -0700

    Added colors package

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 // Import the Mongoose library, which is our Object Data Modeling (ODM) tool for MongoDB.
 const mongoose=require('mongoose');
 //import color package
-
+const colors=require('colors');
 // Define an asynchronous function to connect to the database.
 const connectDB=async()=>{
     try{
@@ -11,9 +11,9 @@ const connectDB=async()=>{
         //  The dotenv package (which we'll call in server.js) makes this variable available.
         const conn=await mongoose.connect(process.env.MONGO_URI);
         // We use the 'conn' object to access details about the connection, like the host name.
-        console.log(`mongoDB connected:${conn.connection.host}`);
+        console.log(`mongoDB connected:${conn.connection.host}`.cyan.underline);
     }catch(error){
-        console.error(`error: ${error.message}`);
+        console.error(`error: ${error.message}`.red.bold);
             // Exit the Node.js process with a "failure" code (1).
            // This is crucial because if the app can't connect to its database,
            // it's fundamentally broken and should not continue to run.
